71175ac9d3691f9c776e68e71ba5fc6b25e4b560
Mardino229 <maxdassi01@gmail.com>
2025-02-08 13:29:14 +0100

    Correction sur l'api

--- app/Http/Requests/ElectionRequest.php
+++ app/Http/Requests/ElectionRequest.php
@@ -23,7 +23,8 @@ class ElectionRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'start_date' => 'required|date|after:today',
+//            'start_date' => 'required|date|after:today',
+            'start_date' => 'required|date',
             'end_date' => 'required|date|after_or_equal:start_date',
         ];
     }
