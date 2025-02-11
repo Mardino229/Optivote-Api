dd36fff744927dc2702aaa48e7ddd6d68169650b
Mardino229 <maxdassi01@gmail.com>
2025-02-11 07:54:18 +0100

    Correction sur l'api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <link rel="icon" href="{{ asset('optivote.png') }}" type="image/x-icon">
+        <title>Optivote</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
