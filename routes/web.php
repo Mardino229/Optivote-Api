cee5fb40f066176f5623db4c54b0a801bc68adb3
Mardino229 <maxdassi01@gmail.com>
2025-02-11 13:08:19 +0100

    Correction sur l'api

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PersonController::class, "index"])->name("create");
 
 
-Route::post('/create', [PersonController::class, "store"])->name("store");
+//Route::post('/create', [PersonController::class, "store"])->name("store");
 
 
 Route::get('/create', [PersonController::class, "create"])->name("create");
